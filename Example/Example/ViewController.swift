cadb93ca38086761ebc9d6ca490187e563f3273e
Sean Rucker <sean@rover.io>
2019-04-30 17:45:58 -0400

    Example app from stock single view application template

---
+++ Example/Example/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Example
+//
+//  Created by Sean Rucker on 2019-04-30.
+//  Copyright © 2019 Rover Labs Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
