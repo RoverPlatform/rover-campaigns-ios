07638bb7f4d70efa7aba9b5bb759ab96d8978831
Andrew Clunis <andrew@rover.io>
2019-09-04 15:32:57 -0400

    Make GraphQL caching easier by ensuring JSON keys in requests are consistent ordered.

--- Sources/Foundation/Extensions/Foundation.JSONEncoder.swift
+++ Sources/Foundation/Extensions/Foundation.JSONEncoder.swift
@@ -12,6 +12,10 @@ extension JSONEncoder {
     public static let `default`: JSONEncoder = {
         let encoder = JSONEncoder()
         encoder.dateEncodingStrategy = .formatted(DateFormatter.rfc3339)
+        if #available(iOS 11.0, *) {
+            // Stable ordering of the keys is very helpful with GraphQL caching on the cloud API side.
+            encoder.outputFormatting = [.sortedKeys]
+        }
         return encoder
     }()
 }
