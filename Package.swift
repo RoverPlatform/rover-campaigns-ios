179ee248856a45bee94b86893443eca7b0e946c2
Andrew Clunis <andrew@rover.io>
2019-09-13 13:29:53 -0400

    Rover to 3.5.0 on SwiftPM.

--- Package.swift
+++ Package.swift
@@ -48,7 +48,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/RoverPlatform/rover-ios", .branch("feature/swift-pm"))
+        .package(url: "https://github.com/RoverPlatform/rover-ios", from: "3.5.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
