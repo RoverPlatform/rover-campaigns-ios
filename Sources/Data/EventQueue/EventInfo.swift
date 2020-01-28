173d1f15d239bc182bfd3be4e7c410c988bbcd70
Andrew Clunis <andrew@rover.io>
2020-01-28 14:52:46 -0500

    Support for Screen Viewed Event for Advanced Data Package
    Resolves #90.

--- Sources/Data/EventQueue/EventInfo.swift
+++ Sources/Data/EventQueue/EventInfo.swift
@@ -24,3 +24,30 @@ public struct EventInfo {
         self.timestamp = timestamp
     }
 }
+
+public extension EventInfo {
+    /// Create an EventInfo that tracks a Screen Viewed event for Analytics use.  Use it for tracking screens & content in your app belonging to components other than Rover.
+    init(
+        forViewingScreenWithName screenName: String,
+        screenLabel: String? = nil,
+        contentID: String? = nil
+    ) {
+        var attributes = [
+            "screenName": screenName
+        ]
+        
+        if let screenLabel = screenLabel {
+            attributes["screenLabel"] = screenLabel
+        }
+        
+        if let contentID = contentID {
+            attributes["contentID"] = contentID
+        }
+        
+        self.name = "Screen Viewed"
+        // using a nil namespace to represent events for screens owned by the app vendor.
+        self.namespace = nil
+        self.attributes = Attributes(rawValue: attributes)
+        self.timestamp = Date()
+    }
+}
